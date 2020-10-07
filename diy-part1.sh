88812c9a5f049021c4d1dd5c757738db1ac6c1b9
leesuncom <70786826+leesuncom@users.noreply.github.com>
2020-10-07 13:10:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,11 +10,15 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 # Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-cd package
-git clone https://github.com/kenzok8/openwrt-packages.git
-git clone https://github.com/kenzok8/small.git
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+rm -rf ../lean/luci-theme-argon
+
+# cd package
+# git clone https://github.com/kenzok8/openwrt-packages.git
+# git clone https://github.com/kenzok8/small.git

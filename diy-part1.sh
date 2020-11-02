63bc9bee0640bf5036b6fb0a2b5d4d581ca8564c
leesuncom <70786826+leesuncom@users.noreply.github.com>
2020-11-02 16:23:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+#=============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Uncomment a feed source
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+# ACRH17 KERNEL 5.4
+#=============================================================
+
+# 启用 fw876/helloworld
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# 增加源
+# sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package' feeds.conf.default
 
-# Add luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
-rm -rf ../lean/luci-theme-argon
-
-# cd package
-# git clone https://github.com/kenzok8/openwrt-packages.git
 # git clone https://github.com/kenzok8/small.git

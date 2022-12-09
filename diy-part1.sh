5d056606b2a09137857157c8ec3fca25641db2b2
leesuncom <70786826+leesuncom@users.noreply.github.com>
2022-12-09 20:32:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,6 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.con
 
 rm -rf package/small-package/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/small-package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
-# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-git clone https://github.com/kenzok8/small-package package/small-package
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 git clone https://github.com/kenzok8/small.git package/small
 git clone https://github.com/open-mesh-mirror/batman-adv.git package/batman-adv

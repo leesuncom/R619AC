5c25aebcb8944db1b2bb6b6d0c195fcbcec8f81d
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-01-13 16:27:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,8 @@
 
 # Add a feed source
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 git clone https://github.com/kenzok8/small.git package/small
@@ -19,5 +21,3 @@ git clone https://github.com/open-mesh-mirror/batman-adv.git package/batman-adv
 cd package/openwrt-packages  
 rm -rf luci-theme-argon  
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
-# rm -rf luci-app-netdata
-# git clone https://github.com/sirpdboy/luci-app-netdata.git

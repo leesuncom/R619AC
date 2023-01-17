f164aef7b0e045a28e5f4c949e5e5d84e59883f2
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-01-17 18:01:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@ git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packag
 git clone https://github.com/kenzok8/small.git package/small
 # git clone https://github.com/open-mesh-mirror/batman-adv.git package/batman-adv
 # rm -rf package/openwrt-packages/luci-app-adguardhome && git clone https://github.com/OldCoding/luci-app-adguardhome.git package/openwrt-packages/luci-app-adguardhome
-rm -rf package/openwrt-packages/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/openwrt-packages/luci-theme-argon
+# rm -rf package/openwrt-packages/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/openwrt-packages/luci-theme-argon
 # rm -rf package/lean/luci-app-netdata && git clone https://github.com/sirpdboy/luci-app-netdata.git package/openwrt-packages/luci-app-netdata

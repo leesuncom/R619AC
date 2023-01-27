520866dfb6e65f376b445695fa3998fd3619cf10
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-01-27 23:48:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,6 @@ git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packag
 git clone https://github.com/kenzok8/small.git package/small
 git clone https://github.com/sirpdboy/luci-app-wizard package/openwrt-packages/luci-app-wizard
 git clone https://github.com/sirpdboy/luci-app-autotimeset package/openwrt-packages/luci-app-autotimeset
-git clone https://github.com/sirpdboy/luci-app-advanced.git package/openwrt-packages/luci-app-advanced
 rm -rf package/openwrt-packages/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/openwrt-packages/luci-theme-argon
 # git clone https://github.com/open-mesh-mirror/batman-adv.git package/batman-adv
 # rm -rf package/openwrt-packages/luci-app-adguardhome && git clone https://github.com/OldCoding/luci-app-adguardhome.git package/openwrt-packages/luci-app-adguardhome

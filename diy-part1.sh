fc1a0ed3fb3484cfd8750c847f3b8c02338a5051
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-03-03 13:05:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,10 +12,10 @@
 
 # Add a feed source
 # sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
-git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-# git clone https://github.com/kenzok8/small-package package/small-package
+# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+git clone https://github.com/kenzok8/small-package package/small-package
 # git clone https://github.com/kenzok8/small.git package/small
-rm -rf package/openwrt-packages/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/openwrt-packages/luci-theme-argon
+rm -rf package/small-package/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/small-package/luci-theme-argon
 # git clone https://github.com/open-mesh-mirror/batman-adv.git package/batman-adv
 # rm -rf package/openwrt-packages/luci-app-adguardhome && git clone https://github.com/OldCoding/luci-app-adguardhome.git package/openwrt-packages/luci-app-adguardhome
 # rm -rf package/lean/luci-app-netdata && git clone https://github.com/sirpdboy/luci-app-netdata.git package/openwrt-packages/luci-app-netdata

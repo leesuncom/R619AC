e5c9dd18f9b0f78e73bf687a903f180341ad5f0d
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-03-22 09:50:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,7 @@
 
 # Add a feed source
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add theme
 rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
@@ -21,6 +22,5 @@ rm -rf package/lean/luci-theme-neobird && git clone https://github.com/thinktip/
 # Add app
 git clone https://github.com/open-mesh-mirror/batman-adv.git package/batman-adv
 git clone -b lede https://github.com/pymumu/luci-app-smartdns package/lean/luci-app-smartdns
-git clone https://github.com/xiaorouji/openwrt-passwall
 svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall package/lean/luci-app-passwall
 # svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/lean/luci-app-adguardhome

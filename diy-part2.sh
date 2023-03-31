ef7d1244f0973ec7c67c2b5f8058f6041271be83
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-03-31 09:24:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,5 @@
 # Modify default IP
 sed '10 iuci\ set\ system.@system[0].hostname=NeoBird' -i package/lean/default-settings/files/zzz-default-settings
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+sed -i "s/OpenWrt /Wing build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "/firewall\.user/d" package/lean/default-settings/files/zzz-default-settings

d7ea1774ef7527178952aa056b429149e91f48e5
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-04-26 16:10:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,8 +10,20 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
+# 网络配置信息，将从 zzz-default-settings 文件的第2行开始添加
+sed -i "2i # network config" ./package/lean/default-settings/files/zzz-default-settings
+sed -i "3i uci set network.lan.ipaddr='192.168.1.2'" ./package/lean/default-settings/files/zzz-default-settings # 默认IP地址，旁路由时不会和主路由的 192.168.1.1 冲突
+sed -i "4i uci set network.lan.proto='static'" ./package/lean/default-settings/files/zzz-default-settings # 静态 IP
+sed -i "5i uci set network.lan.type='bridge'" ./package/lean/default-settings/files/zzz-default-settings  # 接口类型：桥接
+sed -i "6i uci set network.lan.ifname='eth0.1'" ./package/lean/default-settings/files/zzz-default-settings  # 网络端口：默认 eth0.1，第一个接口
+sed -i "7i uci set network.lan.netmask='255.255.255.0'" ./package/lean/default-settings/files/zzz-default-settings    # 子网掩码
+sed -i "8i uci set network.lan.gateway='192.168.1.1'" ./package/lean/default-settings/files/zzz-default-settings  # 默认网关地址（主路由 IP）
+sed -i "9i uci set network.lan.dns='192.168.1.1'" ./package/lean/default-settings/files/zzz-default-settings  # 默认上游 DNS 地址
+sed -i "10i uci set network.wan.proto='none'" # 关掉wan  
+sed -i "11i uci set dhcp.lan.ignore='1'" # 关掉lan的dhcp 
+sed -i "12i uci set wireless.@wifi-device[0].disabled='1'" # 关闭无线
+sed -i "13i uci commit network\n" ./package/lean/default-settings/files/zzz-default-settings
 sed '6 iuci\ set\ system.@system[0].hostname=NeoBird' -i package/lean/default-settings/files/zzz-default-settings
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 sed -i "s/OpenWrt /Wing build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 sed -i "/firewall\.user/d" package/lean/default-settings/files/zzz-default-settings

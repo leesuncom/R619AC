d0164e7e82056d8a2ca73e5b7cafd85603ba16bd
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-04-28 23:52:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,7 +20,7 @@ rm -rf package/lean/luci-theme-neobird && git clone https://github.com/thinktip/
 
 # Add app
 git clone -b lede https://github.com/pymumu/luci-app-smartdns package/lean/luci-app-smartdns
-git clone https://github.com/leesuncom/smartdns.git package/lean/smartdns
+rm -rf package/lean/smartdns && git clone https://github.com/leesuncom/smartdns.git package/lean/smartdns
 # svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns package/lean/luci-app-smartdns
 # svn export https://github.com/kenzok8/openwrt-packages/trunk/smartdns package/lean/smartdns
 # svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/lean/luci-app-adguardhome

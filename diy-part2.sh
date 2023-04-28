031b939afc7c8bd6baa3884c536a683347b9ce9a
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-04-28 20:33:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,13 @@
 #
 
 # Modify default IP
+sed -i "17i sed -i '/option type/d' /etc/config/network" package/lean/default-settings/files/zzz-default-settings
+sed -i "18i uci set network.lan.ifname=eth0.1" package/lean/default-settings/files/zzz-default-settings
+sed -i "19i uci set network.lan.netmask=255.255.255.0" package/lean/default-settings/files/zzz-default-settings
+sed -i "20i uci set network.lan.gateway=192.168.1.1" package/lean/default-settings/files/zzz-default-settings
+sed -i "21i uci set network.lan.dns=127.0.0.1" package/lean/default-settings/files/zzz-default-settings 
+sed -i "22i uci commit network" package/lean/default-settings/files/zzz-default-settings
+
 sed -i '5i uci set system.@system[0].hostname=NeoBird' package/lean/default-settings/files/zzz-default-settings 
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 sed -i "s/OpenWrt /Wing build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings

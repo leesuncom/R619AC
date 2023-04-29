08e9d8948a41136261ceca9c568249506ec44608
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-04-29 09:41:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,17 +21,17 @@ sed -i "23i sed -i '/option ip6assign/d' /etc/config/network" package/lean/defau
 sed -i "24i sed -i '/option proto dhcpv6/d' /etc/config/network" package/lean/default-settings/files/zzz-default-settings
 sed -i "25i uci commit network" package/lean/default-settings/files/zzz-default-settings
 
-sed -i "26i sed -i '/option ra_management 1/d' /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
-sed -i "27i sed -i '/option dhcpv6 server/d' /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
-sed -i "28i sed -i '/option ra server/d' /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+sed -i "26i sed -i '/option ra_management/d' /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+sed -i "27i sed -i '/option dhcpv6/d' /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+sed -i "28i sed -i '/option ra/d' /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
 sed -i "29i uci set dhcp.lan.ignore=1" package/lean/default-settings/files/zzz-default-settings
 sed -i "30i uci commit dhcp" package/lean/default-settings/files/zzz-default-settings
 
-#sed -i "31i sed -i '165,170d' /etc/config/firewall" package/lean/default-settings/files/zzz-default-settings
-#sed -i "32i sed -i '18,134d' /etc/config/firewall" package/lean/default-settings/files/zzz-default-settings
-#sed -i "33i sed -i '/option syn_flood 1/d' /etc/config/firewall" package/lean/default-settings/files/zzz-default-settings
-#sed -i "34i sed -i '/option forward 'ACCEPT'/a\option masq 1' /etc/config/firewall" package/lean/default-settings/files/zzz-default-settings
-#sed -i "35i uci commit firewall" package/lean/default-settings/files/zzz-default-settings
+sed -i "31i sed -i '165,170d' /etc/config/firewall" package/lean/default-settings/files/zzz-default-settings
+sed -i "32i sed -i '18,134d' /etc/config/firewall" package/lean/default-settings/files/zzz-default-settings
+sed -i "33i sed -i '/option syn_flood/d' /etc/config/firewall" package/lean/default-settings/files/zzz-default-settings
+sed -i "34i sed -i '/config zone/a\option masq 1' /etc/config/firewall" package/lean/default-settings/files/zzz-default-settings
+sed -i "35i uci commit firewall" package/lean/default-settings/files/zzz-default-settings
 
 sed -i '5i uci set system.@system[0].hostname=NeoBird' package/lean/default-settings/files/zzz-default-settings 
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate

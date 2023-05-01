2bf69183ab21f19dbd1292105f0805753db41314
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-05-01 22:24:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,12 +11,20 @@
 #
 
 # Modify default IP
+
+sed -i "19i sed -i '/option ip6assign/d' /etc/config/network" package/lean/default-settings/files/zzz-default-settings
 sed -i "20i uci set network.lan.gateway=192.168.1.1" package/lean/default-settings/files/zzz-default-settings
 sed -i "21i uci set network.lan.dns=127.0.0.1" package/lean/default-settings/files/zzz-default-settings
 sed -i "22i uci commit network" package/lean/default-settings/files/zzz-default-settings
 
 sed -i "23i uci set wireless.@wifi-device[0].disabled=1" package/lean/default-settings/files/zzz-default-settings
 sed -i "24i uci set wireless.@wifi-device[1].disabled=1" package/lean/default-settings/files/zzz-default-settings
+
+sed -i "25i sed -i '/option ra_management/d' /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+sed -i "26i sed -i '/option dhcpv6/d' /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+sed -i "27i sed -i '/option ra/d' /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+sed -i "28i uci set dhcp.lan.ignore=1" package/lean/default-settings/files/zzz-default-settings
+sed -i "29i uci commit dhcp" package/lean/default-settings/files/zzz-default-settings
 
 sed '10 iuci\ set\ system.@system[0].hostname=NeoBird' -i package/lean/default-settings/files/zzz-default-settings
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate

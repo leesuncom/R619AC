8a8b23bb583f5b7b03f3e41eb883bae0c546f4dd
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-05-03 12:26:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -31,5 +31,5 @@ sed -i "30i chmod 755 /etc/smartdns/up.sh" package/lean/default-settings/files/z
 sed '10 iuci\ set\ system.@system[0].hostname=NeoBird' -i package/lean/default-settings/files/zzz-default-settings
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 sed -i "s/OpenWrt /Wing build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-sed -i "46i echo 'iptables -t nat -I POSTROUTING -o eth0.1 -j MASQUERADE' >> /etc/firewall.user" package/lean/default-settings/files/zzz-default-settings
-# sed -i "/firewall\.user/d" package/lean/default-settings/files/zzz-default-settings
+# sed -i "46i echo 'iptables -t nat -I POSTROUTING -o eth0.1 -j MASQUERADE' >> /etc/firewall.user" package/lean/default-settings/files/zzz-default-settings
+sed -i "/firewall\.user/d" package/lean/default-settings/files/zzz-default-settings

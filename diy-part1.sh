f46669c34f0b60de77eabed60eb3576752d40ed8
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-05-20 10:52:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,9 +18,12 @@ ls
 cd package
 mkdir openwrt-packages
 cd openwrt-packages
-git clone -b lede https://github.com/pymumu/luci-app-smartdns
+# git clone -b lede https://github.com/pymumu/luci-app-smartdns
 # svn export https://github.com/leesuncom/packages/trunk/net/smartdns
 
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5
+git clone https://github.com/sbwml/v2ray-geodata
 # Add theme
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config.git

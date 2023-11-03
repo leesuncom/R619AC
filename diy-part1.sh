40a771d3f58794ae9c85529dc991434e176d8ad7
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-11-03 18:06:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,13 +11,13 @@
 #
 
 # Add a feed source
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
+sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
 
 # Add app
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns package/lean/luci-app-smartdns
 # svn export https://github.com/leesuncom/packages/trunk/net/smartdns package/lean/smartdns
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/lean/luci-app-mosdns
-rm -rf feeds/packages/net/v2ray-geodata && git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata package/lean/v2ray-geodata
+# rm -rf feeds/packages/net/v2ray-geodata && git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata package/lean/v2ray-geodata
 # Add theme
-rm -rf feeds/luci/applications/luci-app-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-rm -rf feeds/luci/applications/luci-app-argon-config && git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
+# rm -rf feeds/luci/applications/luci-app-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+# rm -rf feeds/luci/applications/luci-app-argon-config && git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config

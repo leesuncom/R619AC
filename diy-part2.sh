08297b5a4f5d53d8abae0d9192e4ab50278b8882
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-11-04 10:03:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
-# sed -i 's/OpenWrt/NeoBird-R619AC/g' ./package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/NeoBird/g' ./package/base-files/files/bin/config_generate
 
 # echo "修改wifi名称"
 # sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh

8b605ef5079497db091b47b00665148e97c40fe9
leesuncom <70786826+leesuncom@users.noreply.github.com>
2024-04-13 14:47:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,14 +11,23 @@
 #
 
 # Add a feed source
-sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git;master' feeds.conf.default
+sed -i "/telephony/d" feeds.conf.default
+
+sed -i "s?targets/%S/packages?targets/%S/\$(LINUX_VERSION)?" include/feeds.mk
+
+sed -i '/	refresh_config();/d' scripts/feeds
+
+./scripts/feeds update -a
+./scripts/feeds install -a -p kiddin9 -f
+./scripts/feeds install -a
 
 # Add app
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns package/lean/luci-app-smartdns
 # svn export https://github.com/leesuncom/packages/trunk/net/smartdns package/lean/smartdns
-rm -rf package/lean/luci-app-mosdns/luci-app-mosdns
-rm -rf package/lean/luci-app-mosdns/mosdns
-rm -rf package/lean/luci-app-mosdns/v2dat
+# rm -rf package/lean/luci-app-mosdns/luci-app-mosdns
+# rm -rf package/lean/luci-app-mosdns/mosdns
+# rm -rf package/lean/luci-app-mosdns/v2dat
 # git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/lean/luci-app-mosdns
 # rm -rf feeds/packages/net/v2ray-geodata && git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata package/lean/v2ray-geodata
 # Add theme

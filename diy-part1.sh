99fed55f33f2f32ed46ec9f74621fb5eebe43e96
leesuncom <70786826+leesuncom@users.noreply.github.com>
2024-04-13 18:13:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,14 +12,16 @@
 
 # Add a feed source
   sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-  # rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
-
+  rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+  rm -rf feeds/luci/applications/luci-app-smartdns
+  rm -rf feeds/packages/net/smartdns
+  
 # Add app
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns package/lean/luci-app-smartdns
 # svn export https://github.com/leesuncom/packages/trunk/net/smartdns package/lean/smartdns
-rm -rf package/lean/luci-app-mosdns/luci-app-mosdns
-rm -rf package/lean/luci-app-mosdns/mosdns
-rm -rf package/lean/luci-app-mosdns/v2dat
+# rm -rf package/lean/luci-app-mosdns/luci-app-mosdns
+# rm -rf package/lean/luci-app-mosdns/mosdns
+# rm -rf package/lean/luci-app-mosdns/v2dat
 # git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/lean/luci-app-mosdns
 # rm -rf feeds/packages/net/v2ray-geodata && git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata package/lean/v2ray-geodata
 # Add theme

21c6dd0a29f8b41ab4ae580e235699a12b0e83c1
leesuncom <70786826+leesuncom@users.noreply.github.com>
2024-04-15 09:30:18 +0800

    Create diy-2.sh

---
+++ diy-2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# 发布固件名称添加日期
+sed -i 's/^IMG_PREFIX\:\=.*/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)-$(shell TZ=UTC-8 date +"%Y.%m.%d-%H%M")-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)$(BOARD)$(if $(SUBTARGET),-$(SUBTARGET))/g' include/image.mk
+
+# 页面版本号添加日期
+sed -i "s/R[0-9]\+\.[0-9]\+\.[0-9]\+/&("$(date +%Y-%m-%d)")/g" package/lean/default-settings/files/zzz-default-settings
+
+# 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i 's/OpenWrt/NeoBird/g' ./package/base-files/files/bin/config_generate
+
+# 去除默认bootstrap主题
+sed -i 's/[b|B]ootstrap/argon/g' ./feeds/luci/collections/luci/Makefile
+
+# echo "修改wifi名称"
+# sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+# sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' ./package/lean/default-settings/files/zzz-default-settings
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate

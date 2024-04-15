4bf71f1813dc7e0f4facc917bb28152bc906ac4d
leesuncom <70786826+leesuncom@users.noreply.github.com>
2024-04-15 09:29:04 +0800

    Create diy-1.sh

---
+++ diy-1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Add a feed source
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default

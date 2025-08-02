ed7afbcf99635bd2fdbd76c72d8ffea0999f742a
leesuncom <70786826+leesuncom@users.noreply.github.com>
2025-08-02 16:22:30 +0800

    Update R70001.sh

--- R70001.sh
+++ R70001.sh
@@ -11,7 +11,16 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+cd openwrt
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+rm -rf feeds/luci/applications/luci-app-openclash
+cp -f feeds/smpackage/.github/diy/default-settings package/lean/default-settings/files/zzz-default-settings
+cp -f feeds/smpackage/.github/diy/banner package/base-files/files/etc/banner
+#cp -f feeds/smpackage/.github/diy/index_x86.htm package/lean/autocore/files/x86/index.htm
+sed -i "s/%D %V, %C/openwrt $(date +'%m.%d') by kenzo/g" package/base-files/files/etc/banner
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default

d14f20a8dcb98be221693054639718a193d606b8
leesuncom <70786826+leesuncom@users.noreply.github.com>
2025-08-02 17:13:07 +0800

    Update R70001.sh

--- R70001.sh
+++ R70001.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #===============================================
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.1.2/g' openwrt/package/base-files/files/bin/config_generate
+
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile

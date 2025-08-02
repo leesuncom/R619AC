857f92b1bbdea543919a609ec6658976cecde5de
leesuncom <70786826+leesuncom@users.noreply.github.com>
2025-08-02 11:08:03 +0800

    Update R70001.sh

--- R70001.sh
+++ R70001.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git istore https://github.com/linkease/istore;main' >>feeds.conf.default
 #echo 'src-git weifuture https://github.com/hongweifuture/openwrt-packages' >>feeds.conf.default

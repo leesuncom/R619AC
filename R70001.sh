c0acd1674a31035311fa0a501f3cacf2f9c6e0c5
leesuncom <70786826+leesuncom@users.noreply.github.com>
2025-08-03 21:19:26 +0800

    Update R70001.sh

--- R70001.sh
+++ R70001.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #===============================================
 
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 
 # Modify default theme

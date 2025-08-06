5d273a07a43b858e7ca6d047f01432217a789f34
leesuncom <70786826+leesuncom@users.noreply.github.com>
2025-08-06 16:14:29 +0800

    Update and rename R70002.sh to diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# replace luci-theme-argon to lastest update
+rm -rf feeds/kenzo/luci-theme-argon feeds/kenzo/luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/kenzo/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git feeds/kenzo/luci-app-argon-config
+
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/utils/v2dat
+
+# replace smartdns to lastest update
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns}
+rm -rf feeds/luci/applications/luci-app-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns
+sed -i 's/1.2024.45/1.2024.46.0.13/g' feeds/packages/net/smartdns/Makefile
+git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
+sed -i 's/1.2024.45/1.2024.46/g' feeds/luci/applications/luci-app-smartdns/Makefile
+
+# goland 2.1 to golang 2.2
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+# miniupnpd 2.3.7 to 2.1.20200510
+#rm -fr feeds/packages/net/miniupnpd
+#git clone --filter=blob:none --no-checkout https://github.com/openwrt/packages miniupnpd-tmp
+#cd miniupnpd-tmp
+#git sparse-checkout init --cone
+#git sparse-checkout set net/miniupnpd
+#git checkout openwrt-18.06
+#cp -r net/miniupnpd ../feeds/packages/net/
+#cd ..
+#rm -fr miniupnpd-tmp

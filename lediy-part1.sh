239bc62b894b337ca66835551d8b18ab376a025a
leesuncom <70786826+leesuncom@users.noreply.github.com>
2025-08-08 22:14:57 +0800

    Update lediy-part1.sh

--- lediy-part1.sh
+++ lediy-part1.sh
@@ -12,28 +12,28 @@
 
 # echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 # replace luci-theme-argon to lastest update
-# rm -rf feeds/kenzo/luci-theme-argon feeds/kenzo/luci-app-argon-config
-# git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/kenzo/luci-theme-argon
-# git clone https://github.com/jerrykuku/luci-app-argon-config.git feeds/kenzo/luci-app-argon-config
+rm -rf feeds/kenzo/luci-theme-argon feeds/kenzo/luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/kenzo/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git feeds/kenzo/luci-app-argon-config
 
 # replace MOSdns to lastest update
-# rm -rf feeds/luci/applications/luci-app-mosdns
-# rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/utils/v2dat
 # rm -rf feeds/packages/net/v2ray-geodata
 # git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 # git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # replace smartdns to lastest update
-# rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns}
-# rm -rf feeds/luci/applications/luci-app-smartdns
-# git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns
-# sed -i 's/1.2024.45/1.2024.46.0.13/g' feeds/packages/net/smartdns/Makefile
-# git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
-# sed -i 's/1.2024.45/1.2024.46/g' feeds/luci/applications/luci-app-smartdns/Makefile
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns}
+rm -rf feeds/luci/applications/luci-app-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns
+sed -i 's/1.2024.45/1.2024.46.0.13/g' feeds/packages/net/smartdns/Makefile
+git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
+sed -i 's/1.2024.45/1.2024.46/g' feeds/luci/applications/luci-app-smartdns/Makefile
 
 # goland 2.1 to golang 2.2
-# rm -rf feeds/packages/lang/golang
-# git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 # miniupnpd 2.3.7 to 2.1.20200510
 #rm -fr feeds/packages/net/miniupnpd

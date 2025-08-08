651e0c99f77259fb62db311d26a5875b1ca94cb9
leesuncom <70786826+leesuncom@users.noreply.github.com>
2025-08-08 12:06:11 +0800

    Update diy-part0.sh

--- diy-part0.sh
+++ diy-part0.sh
@@ -15,7 +15,8 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main
+src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main
 
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default

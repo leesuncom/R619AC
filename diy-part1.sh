f29d41433b4e613a92d7eb4f2badddacb027874d
leesuncom <70786826+leesuncom@users.noreply.github.com>
2025-08-09 16:35:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,11 +24,11 @@ git clone https://github.com/jerrykuku/luci-app-argon-config.git feeds/kenzo/luc
 # git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
 
 # replace MOSdns to lastest update
-#rm -rf feeds/luci/applications/luci-app-mosdns
-# rm -rf feeds/packages/utils/v2dat
-# rm -rf feeds/packages/net/v2ray-geodata
-# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # replace smartdns to lastest update
 rm -rf feeds/packages/net/{alist,adguardhome,smartdns}

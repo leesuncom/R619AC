eb858b180bf46c8442d4800ac861e4f6ba3bb10e
leesuncom <70786826+leesuncom@users.noreply.github.com>
2025-08-09 16:16:28 +0800

    Update lediy-part2.sh

--- lediy-part2.sh
+++ lediy-part2.sh
@@ -13,7 +13,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate
-
+sed -i 's/luci-app-vsftpd//g' lede/include/target.mk
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
